dfd6474936b4823d0886fc2a1c50fc0524979183
Shiva Manjunath <theblogdoctor@gmail.com>
2014-01-06 23:00:18 -0800

    Fixed Issue #31 - A User's FollowedBy property is always Null.

--- src/InstaSharp/Models/Count.cs
+++ src/InstaSharp/Models/Count.cs
@@ -1,7 +1,10 @@
-﻿namespace InstaSharp.Models {
+﻿using Newtonsoft.Json;
+
+namespace InstaSharp.Models {
     public class Count {
         public int Media { get; set; }
         public int Follows { get; set; }
+        [JsonProperty("followed_by")]
         public int FollowedBy { get; set; }
     }
 }
