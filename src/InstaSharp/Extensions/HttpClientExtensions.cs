df1d0c0618da98afa34bb8339e89fa1d3d28f6c1
Fujiy <felipepessoto@gmail.com>
2014-07-26 17:35:40 -0300

    Do not throw on Http Error

--- src/InstaSharp/Extensions/HttpClientExtensions.cs
+++ src/InstaSharp/Extensions/HttpClientExtensions.cs
@@ -1,37 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using InstaSharp.Models.Responses;
+﻿using InstaSharp.Models.Responses;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace InstaSharp.Extensions
 {
     public static class HttpClientExtensions
     {
-        public static async Task<T> ExecuteAsync<T>(this HttpClient client, HttpRequestMessage request)
+        public static async Task<T> ExecuteAsync<T>(this HttpClient client, HttpRequestMessage request) where T : Response
         {
             HttpResponseMessage response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
 
             string resultData = await response.Content.ReadAsStringAsync();
 
-            return JsonConvert.DeserializeObject<T>(resultData);
-        }
-
-        /// <summary>
-        /// Executes async, casts result to <see cref="IResponse"/> on failure preserving meta data error response
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="client"></param>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        public static async Task<T> ExecuteAsyncWithMeta<T>(this HttpClient client, HttpRequestMessage request) where T : Response
-        {
-            var response = await client.SendAsync(request);
-            var resultData = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(resultData);
         }
     }
