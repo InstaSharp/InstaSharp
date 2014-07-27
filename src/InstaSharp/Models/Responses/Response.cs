1a87b1de85f7458341c21c1cb0966065173e0119
Damian Green <damian@gcoders.com>
2014-07-27 19:07:36 +0100

    Renamed Limits properties to be more meaningful

--- src/InstaSharp/Models/Responses/Response.cs
+++ src/InstaSharp/Models/Responses/Response.cs
@@ -8,18 +8,18 @@ namespace InstaSharp.Models.Responses
         public Meta Meta { get; set; }
         public void SetLimits(HttpResponseMessage responseMesssage)
         {
-            RateLimitRemaining = responseMesssage.Headers.GetValues("X-Ratelimit-Remaining").FirstOrDefault();
-            RateLimitLimit = responseMesssage.Headers.GetValues("X-Ratelimit-Limit").FirstOrDefault();
+            CallsRemainingThisHour = responseMesssage.Headers.GetValues("X-Ratelimit-Remaining").FirstOrDefault();
+            TotalHourlyCallLimit = responseMesssage.Headers.GetValues("X-Ratelimit-Limit").FirstOrDefault();
         }
 
         /// <summary>
         /// The remaining number of calls available to your app within the 1-hour window
         /// </summary>
-        public string RateLimitRemaining { get; private set; }
+        public string CallsRemainingThisHour { get; private set; }
 
         /// <summary>
         /// the total number of calls allowed within the 1-hour window
         /// </summary>
-        public string RateLimitLimit { get; private set; }
+        public string TotalHourlyCallLimit { get; private set; }
     }
 }
