e8252055a24b7176515649edf8a17ed1b37d266d
Damian Green <damian@gcoders.com>
2014-07-27 18:58:01 +0100

    Added Time limit head information to base Response class. Renamed Response class removing 'I' prefix

--- src/InstaSharp/Extensions/HttpClientExtensions.cs
+++ src/InstaSharp/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using InstaSharp.Models.Responses;
+using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,13 +7,17 @@ namespace InstaSharp.Extensions
 {
     internal static class HttpClientExtensions
     {
-        public static async Task<T> ExecuteAsync<T>(this HttpClient client, HttpRequestMessage request)
+        public static async Task<T> ExecuteAsync<T>(this HttpClient client, HttpRequestMessage request) // This could be constrained with   where T:Response if OAuth Inherited it
         {
-            HttpResponseMessage response = await client.SendAsync(request);
+            var response = await client.SendAsync(request);
+            var resultData = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(resultData);
 
-            string resultData = await response.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<T>(resultData);
+            if (result is Response)
+            {
+                result.To<Response>().SetLimits(response);
+            }
+            return result;
         }
     }
 }
---
+++ src/InstaSharp/Models/Responses/Response.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Net.Http;
+
+namespace InstaSharp.Models.Responses
+{
+    public abstract class Response
+    {
+        public Meta Meta { get; set; }
+        public void SetLimits(HttpResponseMessage responseMesssage)
+        {
+            RateLimitRemaining = responseMesssage.Headers.GetValues("X-Ratelimit-Remaining").FirstOrDefault();
+            RateLimitLimit = responseMesssage.Headers.GetValues("X-Ratelimit-Limit").FirstOrDefault();
+        }
+
+        /// <summary>
+        /// The remaining number of calls available to your app within the 1-hour window
+        /// </summary>
+        public string RateLimitRemaining { get; private set; }
+
+        /// <summary>
+        /// the total number of calls allowed within the 1-hour window
+        /// </summary>
+        public string RateLimitLimit { get; private set; }
+    }
+}
