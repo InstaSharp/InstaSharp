a53e47c49d36eda9ecb847be9c17a4caa3402d4b
Fujiy <felipepessoto@gmail.com>
2014-08-01 20:55:09 -0300

    RealtimeUpdateItem

---
+++ src/InstaSharp/Models/RealtimeUpdateItem.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace InstaSharp.Models
+{
+    public class RealtimeUpdateItem
+    {
+        [JsonProperty("SubScription_ID")]
+        public int SubScriptionId { get; set; }
+
+        public string Object { get; set; }
+
+        [JsonProperty("Object_ID")]
+        public string ObjectId { get; set; }
+
+        [JsonProperty("Changed_Aspect")]
+        public string ChangedAspect { get; set; }
+
+        public string Time { get; set; }
+    }
+}
