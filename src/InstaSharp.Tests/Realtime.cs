InstaSharp/InstaSharp

commit 7561b0f25c71ef06b4e444ddbaad28acd696f0d7

Subscription uses InstagramAPI base class methods

src/InstaSharp.Tests/Realtime.cs
@@ -1,10 +1,7 @@
-﻿using System.Net;
-using InstaSharp.Models.Responses;
+﻿using InstaSharp.Models.Responses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using Subscription = InstaSharp.Endpoints.Subscription;
 

src/InstaSharp/Endpoints/Subscription.cs
@@ -29,10 +29,12 @@ namespace InstaSharp.Endpoints
         {
         }
 
-        private void AddClientCredentialParameters(HttpRequestMessage request)
+        protected override HttpRequestMessage AddAuth(HttpRequestMessage request)
         {
             request.AddParameter("client_id", InstagramConfig.ClientId);
             request.AddParameter("client_secret", InstagramConfig.ClientSecret);
+
+            return request;
         }
 
         /// <summary>
@@ -44,12 +46,12 @@ namespace InstaSharp.Endpoints
         /// <param name="verifyToken"></param>
         /// <exception cref="ArgumentException">If objectId contains spaces and <see cref="type"/>  is objectTag </exception>
         /// <returns></returns>
-        public Task<SubscriptionResponse> Create(Object type, Aspect aspect, String objectId = null, String verifyToken = null)
+        public Task<SubscriptionResponse> Create(Object type, Aspect aspect, string objectId = null, string verifyToken = null)
         {
-            String searchTerm = null;
-            if ((type == Object.Tag || type == Object.Location))
+            string searchTerm = null;
+            if (type == Object.Tag || type == Object.Location)
             {
-                if (String.IsNullOrWhiteSpace(objectId))
+                if (string.IsNullOrWhiteSpace(objectId))
                 {
                     throw new ArgumentException("objectId must be populated when type is Tag or Location", "objectId");
                 }
@@ -61,26 +63,23 @@ namespace InstaSharp.Endpoints
             }
 
             // create a new guid that uniquely identifies this subscription request
-            verifyToken = String.IsNullOrWhiteSpace(verifyToken) ? Guid.NewGuid().ToString() : verifyToken;
-            var postParams = new List<KeyValuePair<string, string>>
+            verifyToken = string.IsNullOrWhiteSpace(verifyToken) ? Guid.NewGuid().ToString() : verifyToken;
+            var postParams = new Dictionary<string, string>
             {
-                new KeyValuePair<string, string>("client_id", InstagramConfig.ClientId),
-                new KeyValuePair<string, string>("client_secret", InstagramConfig.ClientSecret),
-                new KeyValuePair<string, string>("object", type.ToString().ToLower()),
-                new KeyValuePair<string, string>("aspect", aspect.ToString().ToLower()),
-                new KeyValuePair<string, string>("verify_token", verifyToken),
-                new KeyValuePair<string, string>("callback_url", InstagramConfig.CallbackUri)
+                {"client_id", InstagramConfig.ClientId},
+                {"client_secret", InstagramConfig.ClientSecret},
+                {"object", type.ToString().ToLower()},
+                {"aspect", aspect.ToString().ToLower()},
+                {"verify_token", verifyToken},
+                {"callback_url", InstagramConfig.CallbackUri},
             };
 
-            if ((type == Object.Tag || type == Object.Location))
+            if (type == Object.Tag || type == Object.Location)
             {
-                postParams.Add(new KeyValuePair<string, string>("object_id", searchTerm));
+                postParams["object_id"] = searchTerm;
             }
 
-            var request = new HttpRequestMessage(HttpMethod.Post, Client.BaseAddress)
-            {
-                Content = new FormUrlEncodedContent(postParams)
-            };
+            var request = Request(null, HttpMethod.Post);
 
             return Client.ExecuteAsync<SubscriptionResponse>(request);
         }
@@ -92,9 +91,8 @@ namespace InstaSharp.Endpoints
         /// <returns></returns>
         public Task<SubscriptionResponse> UnsubscribeUser(string id)
         {
-            var request = new HttpRequestMessage { Method = HttpMethod.Delete, RequestUri = Client.BaseAddress };
-
-            AddClientCredentialParameters(request);
+            var request = Request(null, HttpMethod.Delete);
+            
             request.AddParameter("id", id);
 
             return Client.ExecuteAsync<SubscriptionResponse>(request);
@@ -107,9 +105,8 @@ namespace InstaSharp.Endpoints
         /// <returns></returns>
         public Task<SubscriptionResponse> RemoveSubscription(Object type)
         {
-            var request = new HttpRequestMessage { Method = HttpMethod.Delete, RequestUri = Client.BaseAddress };
-
-            AddClientCredentialParameters(request);
+            var request = Request(null, HttpMethod.Delete);
+            
             request.AddParameter("object", type.ToString().ToLower());
 
             return Client.ExecuteAsync<SubscriptionResponse>(request);
@@ -121,9 +118,8 @@ namespace InstaSharp.Endpoints
         /// <returns></returns>
         public Task<SubscriptionResponse> RemoveAllSubscriptions()
         {
-            var request = new HttpRequestMessage { Method = HttpMethod.Delete, RequestUri = Client.BaseAddress };
+            var request = Request(null, HttpMethod.Delete);
 
-            AddClientCredentialParameters(request);
             request.AddParameter("object", "all");
 
             return Client.ExecuteAsync<SubscriptionResponse>(request);
@@ -135,9 +131,7 @@ namespace InstaSharp.Endpoints
         /// <returns></returns>
         public Task<SubscriptionResponse> ListAllSubscriptions()
         {
-            var request = new HttpRequestMessage { Method = HttpMethod.Get, RequestUri = Client.BaseAddress };
-            AddClientCredentialParameters(request);
-
+            var request = Request(null);
             return Client.ExecuteAsync<SubscriptionResponse>(request);
         }
     }

src/InstaSharp/Endpoints/InstagramAPI.cs
@@ -14,7 +14,12 @@ namespace InstaSharp.Endpoints
 
         internal HttpClient Client { get; private set; }
 
-        public InstagramApi(string endpoint, InstagramConfig instagramConfig, OAuthResponse oauthResponse = null)
+        public InstagramApi(string endpoint, InstagramConfig instagramConfig) : this(endpoint, instagramConfig, null)
+        {
+            
+        }
+
+        public InstagramApi(string endpoint, InstagramConfig instagramConfig, OAuthResponse oauthResponse)
         {
             InstagramConfig = instagramConfig;
             OAuthResponse = oauthResponse;
@@ -48,7 +53,7 @@ namespace InstaSharp.Endpoints
             return Request(fragment, HttpMethod.Get);
         }
 
-        private HttpRequestMessage AddAuth(HttpRequestMessage request)
+        protected virtual HttpRequestMessage AddAuth(HttpRequestMessage request)
         {
             if (OAuthResponse == null)
             {

commit e65c4802d01a2b1a073adf5d39e236000833cb80

Broke down subscription Creation by object type Fixed bug introduced by commit 7561b0f25c71ef06b4e444ddbaad28acd696f0d7 where postparams were not being sent

src/InstaSharp.Tests/Realtime.cs
@@ -54,7 +54,7 @@ namespace InstaSharp.Tests
         [TestMethod]
         public async Task SubscribeTag_WithNoClientSecret()
         {
-            var result = await realtime.Create(Subscription.Object.Tag, Subscription.Aspect.Media, "csharp");
+            var result = await realtime.CreateTag("csharp");
             AssertMissingClientSecretUrlParameter(result);
             // This is where Instagram tries to call your callback, without implementing the pubhubsub implementatin that authenticates, it will fail
         }
@@ -72,7 +72,7 @@ namespace InstaSharp.Tests
         [TestMethod]
         public async Task SubscribeUser_WithNoClientSecret()
         {
-            var result = await realtime.Create(Subscription.Object.User, Subscription.Aspect.Media, "joebloggs");
+            var result = await realtime.CreateUser("joebloggs");
             AssertMissingClientSecretUrlParameter(result);
         }
 
@@ -80,7 +80,7 @@ namespace InstaSharp.Tests
         [TestMethod]
         public async Task UnsubscribeUser_WithNoClientSecret()
         {
-            var result = await realtime.UnsubscribeUser("joebloggs");
+            var result = await realtime.RemoveSubscription("joebloggs");
             AssertMissingClientSecretUrlParameter(result);
         }
 

src/InstaSharp/Endpoints/Subscription.cs
@@ -29,7 +29,7 @@ namespace InstaSharp.Endpoints
             /// <summary>
             /// Location
             /// </summary>  
-            Location,   
+            Location,
             /// <summary>
             /// Geography
             /// </summary>
@@ -66,30 +66,88 @@ namespace InstaSharp.Endpoints
         }
 
         /// <summary>
-        /// Create a subscription
+        /// Creates the user subscription.
         /// </summary>
-        /// <param name="type"></param>
-        /// <param name="aspect"></param>
-        /// <param name="objectId">The tag name to subscribe to. This is required, i.e. if <see param="type"/> is<see cref="Object.Tag"/> or <see cref="Object.Location"/></param>
-        /// <param name="verifyToken"></param>
-        /// <exception cref="ArgumentException">If objectId contains spaces and <see param="type"/>  is objectTag </exception>
-        /// <returns>Check the Meta Property for any errors. E.G. Meta.Code == BadRequest, ErrorType="APISubscriptionError" and ErrorMessage="Unable to reach callback URL [url] will be set if the callback url has issues"</returns>
-        public Task<SubscriptionResponse> Create(Object type, Aspect aspect, string objectId = null, string verifyToken = null)
+        /// <param name="verifyToken">The verify token.</param>
+        /// <returns>Check the Meta Property for any errors. E.G. Meta.Code =HttpStatusCode.BadRequest, ErrorType="APISubscriptionError" and ErrorMessage="Unable to reach callback URL [url] will be set if the callback url has issues"</returns>
+        public Task<SubscriptionResponse> CreateUser(String verifyToken = null)
         {
-            string searchTerm = null;
-            if (type == Object.Tag || type == Object.Location)
-            {
-                if (string.IsNullOrWhiteSpace(objectId))
-                {
-                    throw new ArgumentException("objectId must be populated when type is Tag or Location", "objectId");
-                }
-                searchTerm = objectId.Trim().ToLower();
-                if (searchTerm.ContainsWhiteSpace() && type == Object.Tag)
-                {
-                    throw new ArgumentException("subscribing to an objectid with spaces is ignored by Instagram", "objectId");
-                }
-            }
+            var postParams = PostParams(Object.User, verifyToken);
+            return ExecuteAsync(postParams);
+        }
 
+        /// <summary>
+        /// Creates the tag subscription.
+        /// </summary>
+        /// <param name="tag">The hashtag, e.g. 'csharp'</param>
+        /// <param name="verifyToken">The verify token.</param>
+        /// <returns>Check the Meta Property for any errors. E.G. Meta.Code =HttpStatusCode.BadRequest, ErrorType="APISubscriptionError" and ErrorMessage="Unable to reach callback URL [url] will be set if the callback url has issues"</returns>
+
+        public Task<SubscriptionResponse> CreateTag(string tag, String verifyToken = null)
+        {
+            if (string.IsNullOrWhiteSpace(tag))
+            {
+                throw new ArgumentException("tag must be populated", "tag");
+            }
+            var searchTerm = tag.Trim().ToLower();
+            if (searchTerm.ContainsWhiteSpace())
+            {
+                throw new ArgumentException("subscribing to a tag with spaces is ignored by Instagram", "tag");
+            }
+            var postParams = PostParams(Object.Tag, verifyToken);
+            return ExecuteAsync(postParams);
+        }
+
+        /// <summary>
+        /// Creates the location subscription.
+        /// </summary>
+        /// <param name="locationId">The locationId, e.g. '1257285'</param>
+        /// <param name="verifyToken">The verify token.</param>
+        /// <returns>Check the Meta Property for any errors. E.G. Meta.Code =HttpStatusCode.BadRequest, ErrorType="APISubscriptionError" and ErrorMessage="Unable to reach callback URL [url] will be set if the callback url has issues"</returns>
+
+        public Task<SubscriptionResponse> CreateLocation(String locationId, String verifyToken = null)
+        {
+            if (string.IsNullOrWhiteSpace(locationId))
+            {
+                throw new ArgumentException("locationId must be populated", "locationId");
+            }
+            var postParams = PostParams(Object.Location, verifyToken);
+            postParams["object_id"] = locationId;
+            return ExecuteAsync(postParams);
+        }
+        /// <summary>
+        /// Creates the tag subscription.
+        /// </summary>
+        /// <param name="latitude">The latitude.</param>
+        /// <param name="longitude">The longitude.</param>
+        /// <param name="radius">The radius. Must be less than 5000m</param>
+        /// <param name="verifyToken">The verify token.</param>
+        /// <returns>
+        /// Check the Meta Property for any errors. E.G. Meta.Code =HttpStatusCode.BadRequest, ErrorType="APISubscriptionError" and ErrorMessage="Unable to reach callback URL [url] will be set if the callback url has issues"
+        /// </returns>
+        /// <exception cref="System.ArgumentException">locationId must be populated;locationId</exception>
+        public Task<SubscriptionResponse> CreateGeography(float latitude, float longitude, int radius, String verifyToken = null)
+        {
+            if (radius < 0 || radius > 5000)
+            {
+                throw new ArgumentException("radius must be greater than 0 and less tha 5000", "radius");
+            }
+            var postParams = PostParams(Object.Geography, verifyToken);
+            postParams["lat"] = latitude.ToString();
+            postParams["lng"] = latitude.ToString();
+            postParams["radius"] = radius.ToString();
+            return ExecuteAsync(postParams);
+        }
+
+        private Task<SubscriptionResponse> ExecuteAsync(Dictionary<string, string> postParams)
+        {
+            var request = Request(null, HttpMethod.Post);
+            request.Content = new FormUrlEncodedContent(postParams);
+            return Client.ExecuteAsync<SubscriptionResponse>(request);
+        }
+
+        private Dictionary<string, string> PostParams(Object type, string verifyToken)
+        {
             // create a new guid that uniquely identifies this subscription request
             verifyToken = string.IsNullOrWhiteSpace(verifyToken) ? Guid.NewGuid().ToString() : verifyToken;
             var postParams = new Dictionary<string, string>
@@ -97,19 +155,11 @@ namespace InstaSharp.Endpoints
                 {"client_id", InstagramConfig.ClientId},
                 {"client_secret", InstagramConfig.ClientSecret},
                 {"object", type.ToString().ToLower()},
-                {"aspect", aspect.ToString().ToLower()},
+                {"aspect", Aspect.Media.ToString().ToLower()},
                 {"verify_token", verifyToken},
                 {"callback_url", InstagramConfig.CallbackUri},
             };
-
-            if (type == Object.Tag || type == Object.Location)
-            {
-                postParams["object_id"] = searchTerm;
-            }
-
-            var request = Request(null, HttpMethod.Post);
-
-            return Client.ExecuteAsync<SubscriptionResponse>(request);
+            return postParams;
         }
 
         /// <summary>