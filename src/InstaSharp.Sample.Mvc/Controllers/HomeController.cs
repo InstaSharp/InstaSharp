60ef02f7ff832f84620b53684b8e8d3de85a29ef
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2015-09-25 21:35:39 -0300

    ASP.NET MVC Sample

---
+++ src/InstaSharp.Sample.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using InstaSharp.Models.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace InstaSharp.Sample.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        static string clientId = "554dfe9286994bbe98417d8dc7b69a24";
+        static string clientSecret = "";
+        static string redirectUri = "http://localhost:5969/Home/OAuth";
+
+        InstagramConfig config = new InstagramConfig(clientId, clientSecret, redirectUri, "");
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
+
+            if (oAuthResponse == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View(oAuthResponse.User);            
+        }
+
+        public ActionResult Login()
+        {
+            var scopes = new List<OAuth.Scope>();
+            scopes.Add(InstaSharp.OAuth.Scope.Likes);
+            scopes.Add(InstaSharp.OAuth.Scope.Comments);
+
+            var link = InstaSharp.OAuth.AuthLink(config.OAuthUri + "authorize", config.ClientId, config.RedirectUri, scopes, InstaSharp.OAuth.ResponseType.Code);
+
+            return Redirect(link);
+        }
+
+        public async Task<ActionResult> MyFeed()
+        {
+            var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
+
+            if (oAuthResponse == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var users = new Endpoints.Users(config, oAuthResponse);
+
+            var feed = await users.Feed(null, null, null);
+
+            return View(feed.Data);
+        }
+
+        public async Task<ActionResult> OAuth(string code)
+        {
+            // add this code to the auth object
+            var auth = new OAuth(config);
+
+            // now we have to call back to instagram and include the code they gave us
+            // along with our client secret
+            var oauthResponse = await auth.RequestToken(code);
+
+            // both the client secret and the token are considered sensitive data, so we won't be
+            // sending them back to the browser. we'll only store them temporarily.  If a user's session times
+            // out, they will have to click on the authenticate button again - sorry bout yer luck.
+            Session.Add("InstaSharp.AuthInfo", oauthResponse);
+
+            // all done, lets redirect to the home controller which will send some intial data to the app
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/InstaSharp.Sample.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InstaSharp.Sample.Mvc.MvcApplication" Language="C#" %>
