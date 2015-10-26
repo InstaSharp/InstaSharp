c90e3ae95e6b2bd459b32bdab88450bc9af09334
Fadi-git <fadi.alsamman@gmail.com>
2015-10-26 16:12:05 +0300

    Fixing ListAllSubscriptions
    Fixing ListAllSubscriptions by adding the missing SubscriptionsResponse
    which contains a list of subscription data.

---
+++ src/InstaSharp/Models/Responses/SubscriptionsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+namespace InstaSharp.Models.Responses
+{
+    /// <summary>
+    /// Subscriptions Response
+    /// </summary>
+    public class SubscriptionsResponse : Response
+    {
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
+        public List<Subscription> Data { get; set; }
+    }
+}
