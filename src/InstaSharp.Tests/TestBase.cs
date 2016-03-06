99bd6d96ec8af9592a2a20094e4858f346e32101
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-03-06 19:58:05 -0300

    Test new API

--- src/InstaSharp.Tests/TestBase.cs
+++ src/InstaSharp.Tests/TestBase.cs
@@ -4,6 +4,9 @@ using System.Net;
 
 namespace InstaSharp.Tests
 {
+    //InstaSharpTest - 1415228826
+    //InstaSharpTest2 - 3015751092
+
     public class TestBase
     {
         protected readonly OAuthResponse Auth;
@@ -12,26 +15,53 @@ namespace InstaSharp.Tests
 
         protected TestBase()
         {
-            // test account client id
-            Config = new InstagramConfig()
-            {
-                ClientId = "554dfe9286994bbe98417d8dc7b69a24"
-            };
+            bool testingNewApiNov17 = false;
 
-            ConfigWithSecret = new InstagramConfig()
+            if (testingNewApiNov17)
             {
-                ClientId = "554dfe9286994bbe98417d8dc7b69a24",
-                CallbackUri = "https://instasharpapi.azurewebsites.net/Realtime/Callback",
-                ClientSecret = "39de8776637b47d2829cd1a4708ae180"
-            };
+                // test account client id
+                Config = new InstagramConfig()
+                {
+                    ClientId = "fa50f43776ba4cfdaaaa375acc5ccab7"
+                };
+
+                ConfigWithSecret = new InstagramConfig()
+                {
+                    ClientId = "fa50f43776ba4cfdaaaa375acc5ccab7",
+                    CallbackUri = "https://instasharpapi.azurewebsites.net/Realtime/Callback",
+                    ClientSecret = "cd0d5d2f66f146c28cef06d4f4d2dc82"
+                };
 
 
-            // dummy account data. InstaSharpTest
-            Auth = new OAuthResponse()
-            {
-                AccessToken = "1415228826.554dfe9.502432355f084ea581b679a2f94bb350",
-                User = new Models.UserInfo { Id = 1415228826 }
-            };
+                // dummy account data. InstaSharpTest
+                Auth = new OAuthResponse()
+                {
+                    AccessToken = "1415228826.fa50f43.1069f6ca1f734e2f930f70fdc7822885",
+                    User = new Models.UserInfo { Id = 1415228826 }
+                };
+            }
+            else {
+                // test account client id
+                Config = new InstagramConfig()
+                {
+                    ClientId = "554dfe9286994bbe98417d8dc7b69a24"
+                };
+
+                ConfigWithSecret = new InstagramConfig()
+                {
+                    ClientId = "554dfe9286994bbe98417d8dc7b69a24",
+                    CallbackUri = "https://instasharpapi.azurewebsites.net/Realtime/Callback",
+                    ClientSecret = "39de8776637b47d2829cd1a4708ae180"
+                };
+
+
+                // dummy account data. InstaSharpTest
+                Auth = new OAuthResponse()
+                {
+                    AccessToken = "1415228826.554dfe9.502432355f084ea581b679a2f94bb350",
+                    User = new Models.UserInfo { Id = 1415228826 }
+                };
+            }
         }
         protected static void AssertMissingClientSecretUrlParameter(Response result)
         {
