aa63101ae9759701f2772fddd7f05ab79ee8886d
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-11-20 00:12:02 -0200

    Likes API

--- src/InstaSharp.Tests/Likes.cs
+++ src/InstaSharp.Tests/Likes.cs
@@ -2,7 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
-namespace InstaSharp.Tests {
+namespace InstaSharp.Tests
+{
 
     [TestClass]
     public class Likes : TestBase
@@ -16,16 +17,17 @@ namespace InstaSharp.Tests {
         }
 
         [TestMethod, TestCategory("Likes.Get")]
-        public async Task Get() {
-            var result = await likes.Get("555");
+        public async Task Get()
+        {
+            var result = await likes.Get("1387365790411610021_1415228826");
             Assert.IsTrue(result.Meta.Code == HttpStatusCode.OK);
         }
 
         [TestMethod, TestCategory("Likes.PostAndDelete")]
         public async Task PostAndDelete()
         {
-            await likes.Post("649561442972790953_457273003");
-            await likes.Delete("649561442972790953_457273003");
+            var result = await likes.Post("1387365790411610021_1415228826");
+            var result2 = await likes.Delete("1387365790411610021_1415228826");
         }
     }
 }
