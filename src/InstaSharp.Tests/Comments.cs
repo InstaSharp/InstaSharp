485757e3fca85c09d5716f84d29c15a7f6a06640
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-11-20 00:07:03 -0200

    Comments API

--- src/InstaSharp.Tests/Comments.cs
+++ src/InstaSharp.Tests/Comments.cs
@@ -1,28 +1,34 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
 
-namespace InstaSharp.Tests {
+namespace InstaSharp.Tests
+{
     [TestClass]
-    public class Comments : TestBase {
+    public class Comments : TestBase
+    {
         readonly Endpoints.Comments comments;
 
-        public Comments() {
+        public Comments()
+        {
             comments = new Endpoints.Comments(Config, Auth);
         }
 
         [TestMethod, TestCategory("Comments.Get")]
         public async Task Get()
         {
-            var commentResponse = await comments.Get("555");
+            var commentResponse = await comments.Get("1387365790411610021_1415228826");
             Assert.IsTrue(commentResponse.Data.Count > 0);
         }
 
-        // Gotta come back to this one because commenting is white-list only now via API
-        /*[TestMethod, TestCategory("Comments")]
+        [TestMethod, TestCategory("Comments")]
         public async Task Post()
         {
-            var postComment = await comments.Post("555", "Api Test");
+            var postComment = await comments.Post("1387365790411610021_1415228826", "Api Test");
             Assert.AreEqual(postComment.Meta.Code, HttpStatusCode.OK);
-        }*/
+
+            var deleteComment = await comments.Delete("1387365790411610021_1415228826", postComment.Data.Id);
+            Assert.AreEqual(deleteComment.Meta.Code, HttpStatusCode.OK);
+        }
     }
 }
